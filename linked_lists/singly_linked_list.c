25c7064fbb910c20c69e64938b5e13fbf75c7c9b
MateusVasc <mvaraujo2004@gmail.com>
2025-03-10 14:51:55 -0300

    adds Node and Singly Linked List structs

--- linked_lists/singly_linked_list.c
+++ linked_lists/singly_linked_list.c
@@ -1,42 +1,16 @@
 #include <stdlib.h>
 
-typedef struct Singly_linked_list
+typedef struct Node
 {
-    void* data;
-    singly_linked_list* next;
-} singly_linked_list; 
+    int data;
+    struct Node* next;
+} Node;
 
-singly_linked_list* new_singly_linked_list(void* data)
+typedef struct SinglyLinkedList
 {
-    singly_linked_list* node = (singly_linked_list*)malloc(sizeof(singly_linked_list));
-
-    if (node == NULL)
-    {
-        printf("Erro ao alocar memória para o nó\n");
-        exit(EXIT_FAILURE);
-    }
-    
-    node->data = data;
-    node->next = NULL;
-
-    return node;
-}
-
-singly_linked_list* insert_at_beginning(singly_linked_list* head_node, void* new_data)
-{
-    singly_linked_list* new_head = (singly_linked_list*)malloc(sizeof(singly_linked_list));
-
-    if (new_head == NULL)
-    {
-        printf("Erro ao alocar meomória para o nó");
-        exit(EXIT_FAILURE);
-    }
-
-    new_head->data = new_data;
-    new_head->next = head_node;
-    
-    return new_head;
-}
+    Node* head;
+    int size;
+} SinglyLinkedList;
 
 int main()
 {
