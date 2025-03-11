1881729bceee9c26950c535bd68c067ece899cd5
MateusVasc <mvaraujo2004@gmail.com>
2025-03-11 00:00:22 -0300

    adds createList and pushBack methods

--- linked_lists/SinglyLinkedList.java
+++ linked_lists/SinglyLinkedList.java
@@ -35,8 +35,52 @@ public class SinglyLinkedList {
     public int getSize() {
         return size;
     }
+
+    public SinglyLinkedList createList() {
+        return new SinglyLinkedList();
+    }
+
+    public void pushBack(int val) {
+        if (this.size == 0) {
+            this.head = new Node(val);
+            this.size++;
+            return;
+        }
+
+        Node curr = this.head;
+
+        while (curr.next != null) {
+            curr = curr.next;
+        }
+
+        curr.next = new Node(val);
+        this.size++;
+    }
     
     public static void main(String[] args) {
-        System.out.println("Hello There");
+        System.out.println("=== Testing SinglyLinkedList ===");
+    
+        // Creating the list
+        SinglyLinkedList list = new SinglyLinkedList();
+    
+        // Test: Empty list
+        System.out.println("Initial size: " + list.getSize()); // Expected: 0
+    
+        // Adding elements
+        list.pushBack(10);
+        list.pushBack(20);
+        list.pushBack(30);
+    
+        // Checking list size after insertions
+        System.out.println("Size after insertions: " + list.getSize()); // Expected: 3
+    
+        // Traversing and printing the list
+        System.out.print("List elements: ");
+        Node curr = list.getHead();
+        while (curr != null) {
+            System.out.print(curr.data + " -> ");
+            curr = curr.next;
+        }
+        System.out.println("NULL"); // Expected: 10 -> 20 -> 30 -> NULL
     }
 }
